ccef78146ded2f07ab90c74ed873a046a79d270f
Christian <me@rndm.de>
2014-04-07 21:15:22 +0200

    updated broccoli, broccoli-filter `https://github.com/joliss/broccoli/blob/master/docs/hardlink-issue.md`

--- Brocfile.js
+++ Brocfile.js
@@ -1,19 +1,18 @@
 module.exports = function (broccoli) {
-    var eslint = require('./lib/index');
+    var eslint = require('./lib/index'),
+        mergeTrees = require('broccoli-merge-trees');
 
     // lint plugin code
-    var plugin = broccoli.makeTree('lib');
-    plugin = eslint(plugin, {
+    var plugin = eslint('lib', {
         config: './eslint.json'
     });
 
     // lint tests
-    var test = broccoli.makeTree('test');
-    test = eslint(test, {
+    var test = eslint('test', {
         config: './conf/eslint.json',
         rulesdir: './conf/rules',
         format: 'eslint/lib/formatters/compact'
     });
 
-    return [plugin, test];
+    return mergeTrees([plugin, test], { overwrite: true });
 };
