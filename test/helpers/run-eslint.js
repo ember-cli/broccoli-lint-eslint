d1e1e30f1de7ef9db75b5163a8bbf56e7a6bb1a7
Nick Iaconis <niaconis@linkedin.com>
2016-03-10 09:55:30 -0800

    Rework tests

---
+++ test/helpers/run-eslint.js
@@ -0,0 +1,35 @@
+const broccoli = require('broccoli');
+const eslint = require('../../index');
+
+module.exports = function runEslint(path, _options) {
+  const options = _options || {};
+  const buildLog = [];
+  const consoleLog = console.log;
+
+  // stub console.log so we can get the formatter's output
+  console.log = function appendToBuildLog(...args) {
+    const text = args.join(' ');
+
+    buildLog.push(text);
+  };
+
+  // default options
+  options.format = options.format || 'eslint/lib/formatters/compact';
+  options.options = options.options || {};
+  options.options.ignore = options.options.ignore || false;
+
+  const tree = eslint(path, options);
+  const builder = new broccoli.Builder(tree);
+  const promise = builder.build().then(function builderThen() {
+    return buildLog.join('\n');
+  });
+
+  promise.finally(function builderCleanup() {
+    builder.cleanup();
+
+    // restore the original console.log
+    console.log = consoleLog;
+  });
+
+  return promise;
+};
