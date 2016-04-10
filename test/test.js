e669e39210fea8b6148c84b560e2b6215ac4c378
Jonathan Samines <jn.samines@gmail.com>
2016-04-09 20:22:12 -0600

    Feature/update ignore filter message (#30)

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-expressions */
 const fs = require('fs');
+const path = require('path');
 const expect = require('chai').expect;
 const stew = require('broccoli-stew');
 const mv = stew.mv;
@@ -12,12 +13,13 @@ const CONSOLE = '(no-console)';
 const CUSTOM_RULES = 'testing custom rules';
 const DOUBLEQUOTE = 'Strings must use doublequote.';
 const FILEPATH = 'fixture/1.js';
+const TEST_IGNORE_PATH = path.resolve(process.cwd(), './test/fixture/.eslintignore');
 
 describe('EslintValidationFilter', function describeEslintValidationFilter() {
-  function shouldReportErrors(inputTree) {
+  function shouldReportErrors(inputTree, options) {
     return function _shouldReportErrors() {
       // lint test fixtures
-      const promise = runEslint(inputTree);
+      const promise = runEslint(inputTree, options);
 
       return promise.then(function assertLinting({buildLog}) {
         expect(buildLog, 'Used eslint validation').to.have.string(CAMELCASE);
@@ -29,12 +31,20 @@ describe('EslintValidationFilter', function describeEslintValidationFilter() {
     };
   }
 
-  it('should report errors', shouldReportErrors(FIXTURES));
+  // specify test fixtures via a broccoli node
+  const moveTree = mv(new UnwatchedDir(FIXTURES), 'foobar/fixture');
+
+  it('should report errors', shouldReportErrors(FIXTURES, {
+    options: {
+      ignore: false
+    }
+  }));
 
   it('should accept rule paths', function shouldAcceptRulePaths() {
     // lint test fixtures using a custom rule
     const promise = runEslint(FIXTURES, {
       options: {
+        ignore: false,
         rulePaths: ['conf/rules'],
         rules: {
           'custom-no-alert': 2
@@ -47,10 +57,40 @@ describe('EslintValidationFilter', function describeEslintValidationFilter() {
     });
   });
 
+  it('should use a default ignore:true option', function shouldAcceptDefaultIgnore() {
+    const promise = runEslint(FIXTURES, {
+      options: {
+        ignorePath: TEST_IGNORE_PATH
+      }
+    });
+
+    return promise
+      .then(function assertLinting({buildLog}) {
+        expect(buildLog)
+        .to.not.match(/File ignored because of a matching ignore pattern\. Use --no-ignore to override\./);
+      });
+  });
+
+  it('should accept an ignore option', function shouldAcceptIgnoreOption() {
+    const promise = runEslint(FIXTURES, {
+      options: {
+        ignore: true,
+        ignorePath: TEST_IGNORE_PATH
+      }
+    });
+
+    return promise
+      .then(function assertLinting({buildLog}) {
+        expect(buildLog)
+        .to.not.match(/File ignored because of a matching ignore pattern\. Use --no-ignore to override\./);
+      });
+  });
+
   it('should accept config file path', function shouldAcceptConfigFile() {
     // lint test fixtures using a config file at a non-default path
     const promise = runEslint(FIXTURES, {
       options: {
+        ignore: false,
         configFile: 'conf/eslint.json'
       }
     });
@@ -63,6 +103,9 @@ describe('EslintValidationFilter', function describeEslintValidationFilter() {
 
   it('should create test files', function shouldGenerateTests() {
     const promise = runEslint(FIXTURES, {
+      options: {
+        ignore: false
+      },
       testGenerator() {
         return 'test-content';
       }
@@ -75,8 +118,11 @@ describe('EslintValidationFilter', function describeEslintValidationFilter() {
     });
   });
 
-  // specify test fixtures via a broccoli node
-  it('should accept a node as the input', shouldReportErrors(mv(new UnwatchedDir(FIXTURES), 'foobar/fixture')));
+  it('should accept a node as the input', shouldReportErrors(moveTree, {
+    options: {
+      ignore: false
+    }
+  }));
 
   it('should not accept a many:* node as the input', function shouldNotAcceptManyStarNode() {
     expect(() => {
